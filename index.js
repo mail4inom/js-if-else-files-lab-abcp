b50cf70230f2904eee18bb14f8b6b9d25f5248d1
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-29 22:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (name === "Susan"){
 } else{
   console.log("Expected name to be Susan");
 }
-if (height === "70"){
+if (height === 70){
   console.log("The height is correct")
 }else{
   console.log("Expected height to be 70")
